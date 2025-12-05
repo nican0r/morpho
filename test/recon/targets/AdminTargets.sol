60d28d206604212a734ed484da42b47455bef5db
nican0r <94120714+nican0r@users.noreply.github.com>
2025-12-05 10:50:20 +0800

    chore: remove existing artifacts

--- test/recon/targets/AdminTargets.sol
+++ test/recon/targets/AdminTargets.sol
@@ -16,37 +16,6 @@ import {MAX_FEE} from "src/libraries/ConstantsLib.sol";
 abstract contract AdminTargets is BaseTargetFunctions, Properties {
     /// CUSTOM TARGET FUNCTIONS - Add your own target functions here ///
 
-    // Clamped handler for enableIrm
-    function morpho_enableIrm_clamped() public asAdmin {
-        morpho.enableIrm(address(irm));
-    }
-
-    // Clamped handler for enableLltv with 0.5e18
-    function morpho_enableLltv_clamped_05e18() public asAdmin {
-        morpho.enableLltv(0.5e18);
-    }
-
-    // Clamped handler for enableLltv with 0.8e18
-    function morpho_enableLltv_clamped_08e18() public asAdmin {
-        morpho.enableLltv(0.8e18);
-    }
-
-    // Clamped handler for setFee
-    function morpho_setFee_clamped(uint256 newFee_) public asAdmin {
-        uint256 newFee = between(newFee_, 0, MAX_FEE);
-        morpho.setFee(defaultMarketParams, newFee);
-    }
-
-    // Clamped handler for setFeeRecipient
-    function morpho_setFeeRecipient_clamped() public asAdmin {
-        morpho.setFeeRecipient(_getActor());
-    }
-
-    // Clamped handler for setOwner
-    function morpho_setOwner_clamped() public asAdmin {
-        morpho.setOwner(_getActor());
-    }
-
     // Admin-only functions that require owner privileges
     function morpho_enableIrm(address irm) public asAdmin {
         morpho.enableIrm(irm);
